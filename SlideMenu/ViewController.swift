cd15c655773e1679e89e0823b3e8a53d8ec8133e
imran <imufun2@gmail.com>
2019-08-19 08:59:53 -0700

    Initial Commit

---
+++ SlideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideMenu
+//
+//  Created by sinbad on 8/19/19.
+//  Copyright © 2019 sinbad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
